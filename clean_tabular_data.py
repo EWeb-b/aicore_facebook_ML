6e374975841b5452bcac95cf13e4dc93108c83c7
ed <edward.webb@outlook.com>
2022-10-13 12:03:37 +0100

    removed £ sign and commas from 'price' column and converted it into a float type

--- clean_tabular_data.py
+++ clean_tabular_data.py
@@ -1,2 +1,14 @@
 import pandas as pd
 import numpy as np
+
+pd.set_option('display.max_columns', None)
+df = pd.read_csv("data/Products.csv", lineterminator="\n")
+# print(df["product_name"])
+
+# Create a copy to work on.
+df_copy = df.copy()
+
+# Strip the pound sign from price column and convert it into a float type.
+df_copy['price'] = df_copy['price'].str.strip('£')
+df_copy['price'] = df_copy['price'].str.replace(",", "")
+df_copy['price'] = df_copy['price'].astype('float64')
