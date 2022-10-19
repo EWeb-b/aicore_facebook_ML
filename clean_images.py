5d95f274ac0615c18cf1cd28f6204b868b01e14d
ed <edward.webb@outlook.com>
2022-10-19 11:20:15 +0100

    updated the clean_images.py file to use ImageOps.pad

--- clean_images.py
+++ clean_images.py
@@ -2,41 +2,28 @@ import os
 import pandas as pd
 import numpy as np
 from PIL import Image
+from PIL import ImageOps
 
-def clean_image_data(path:str) -> None:
-    """Takes in a filepath to a folder of images, cleans them, then saves them in 'cleaned_images' folder.
+def clean_image_data(final_size: int, path:str) -> None:
+    """Resizes and ensures images are of type RGB, then saves them in 'cleaned_images' folder.
 
     Args:
+        final_size: The width and height of the new image, in pixels.
         path: The path to the folder of images to be cleaned.
     """
     dirs = os.listdir(path)
-    final_size = 512
 
     for item in dirs:
         im = Image.open(path + item)
-        new_im = resize_image(final_size, im)
+        new_im = ImageOps.pad(im, (final_size, final_size))
+        new_im = new_im.convert('RGB')
         new_im.save(f"data/cleaned_images/{item}")
 
-def resize_image(final_size: int, im: Image) -> Image:
-    """Resizes the image and saves it as an RGB type image.
+def check_images(final_size: int) -> None:
+    """Checks the images in the cleaned_images folder and prints to the terminal if they don't have 3 channels and/or not RGB.
 
     Args:
-        final_size: The new size of the horizontal and vertical axes of the image, in pixels.
-        im: The image to be resized.
-
-    Returns:
-        new_im: The new, resized image.
-    """
-    size = im.size
-    ratio = float(final_size) / max(size)
-    new_image_size = tuple([int(x*ratio) for x in size])
-    im = im.resize(new_image_size, Image.ANTIALIAS)
-    new_im = Image.new("RGB", (final_size, final_size))
-    new_im.paste(im, ((final_size-new_image_size[0])//2, (final_size-new_image_size[1])//2))
-    return new_im
-
-def check_images() -> None:
-    """Checks the images in the cleaned_images folder and prints to the terminal if they don't have 3 channels and/or not RGB.
+        final_size: The size the images should be.
     """
     dirs = os.listdir("data/cleaned_images")
     for item in dirs:
@@ -51,13 +38,13 @@ def check_images() -> None:
             print(item)
             print(im.mode)
             print("\n")
-        if im_size != (512,512):
+        if im_size != (final_size, final_size):
             print("Wrong size: ", im_size)
 
 if __name__ == '__main__':
-    path = "data/cleaned_images"
-    clean_image_data("data/images")
-    # check_images()
+    final_size = 128
+    clean_image_data(final_size, "data/images/")
+    #check_images(final_size)
 
 
 
