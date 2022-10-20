5e47541d2810b1bd953d1d95f37cf2b78ddf031a
ed <edward.webb@outlook.com>
2022-10-20 15:32:01 +0100

    finished the simple classifier to classify images according to their category

---
+++ simple_class_model.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+
+from matplotlib.image import imread
+from PIL import Image
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+
+
+class SimpleClassModel():
+    def __init__(self, data):
+        self.df = data
+
+    def get_numpy_representations(self):
+        X_stack = np.stack(self.df['img_arr'].values)
+        X = X_stack.reshape(self.df.shape[0], 128*128*3)
+
+        y = np.stack(self.df['category'].values)
+
+        return X, y
+
+    def run_classification(self):
+        X, y = self.get_numpy_representations()
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+        lr = LogisticRegression()
+        lr.fit(X_train, y_train)
+        prediction = lr.predict(X_test)
+        print("accuracy: ", accuracy_score(y_test, prediction))
+        print("report: ", classification_report(y_test, prediction))
+
+
+if __name__ == "__main__":
+    img_cat_data = pd.read_pickle('data/cl_img_pickle.pkl')
+    classifier = SimpleClassModel(img_cat_data)
+    classifier.run_classification()
+
