bb4036a2bfd7292429c66facfa48f94ccaf34d28
ed <edward.webb@outlook.com>
2022-11-23 17:07:07 +0000

    updated image processor

--- image_classification/image_processor.py
+++ image_classification/image_processor.py
@@ -1,25 +1,23 @@
-import numpy as np
-import pandas as pd
-from PIL import Image
 import torch
 from torchvision import transforms
 
-def process_image(image: Image) -> torch.Tensor:
-
-
-    image = Image.open('data/cleaned_images/0a1baaa8-4556-4e07-a486-599c05cce76c.jpg')
-
-
-    transform = transforms.Compose(
+class ImageProcessor:
+    def __init__(self):
+        self.transform = transforms.Compose(
             [
                 transforms.ToTensor(), # This also changes the pixels to be in range [0, 1] from [0, 255].
-                transforms.RandomCrop((112,112))
+                # transforms.RandomResizedCrop((64,64))
+                transforms.Resize((64,64)),
+                transforms.RandomHorizontalFlip(p=0.3),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ]
-        )
+    )
 
-    im_tensor = transform(image)
-    im_tensor = torch.unsqueeze(im_tensor, 0) # Add the '1' dimension at the start of the tensor.
+    @staticmethod
+    def repeat_channel(x):
+            return x.repeat(3, 1, 1)
 
-    print(im_tensor.shape)
-
-    return im_tensor
+    def __call__(self, image):
+        image = self.transform(image)
+        image = torch.unsqueeze(image, 0) # Add the leading '1' dimension at the start of the tensor.
+        return image
