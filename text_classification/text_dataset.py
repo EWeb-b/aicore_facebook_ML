4537e0abe84f4554160351dcf0b77546f70f1863
ed <edward.webb@outlook.com>
2022-11-23 14:55:27 +0000

    changed the text dataset to use both the name and description of the product

--- text_classification/text_dataset.py
+++ text_classification/text_dataset.py
@@ -3,7 +3,10 @@ import pandas as pd
 import torch
 from transformers import BertTokenizer
 from torch.utils.data import Dataset
-import utilities.clean_tabular_data as cl
+from torch.utils.data import DataLoader
+# import sys
+# sys.path.append("..")
+from ..utilities import clean_tabular_data
 
 class TextDataset(Dataset):
     """ The TextDataset inherits its methods from the
@@ -31,12 +34,15 @@ class TextDataset(Dataset):
         max_length: int, the maximum length of the text going into the
             BERT tokenizer.
     """
-    def __init__(self, filepath: str = 'data/Products.csv', labels_level: int = 0,
-            max_length: int = 50) -> None:
+    def __init__(self,
+                filepath: str = '/home/edwardwebb/Documents/aicore/aicore_facebook_ML/data/Products.csv',
+                labels_level: int = 0,
+                max_length: int = 50) -> None:
         super().__init__()
-        products = cl.clean_tab_data(filepath, labels_level)
+        products = clean_tabular_data.clean_tab_data(filepath, labels_level)
 
         self.data = products
+        self.data['combined'] = products['product_name'].str.cat(products['product_description'], sep=' ')
         self.decoder = dict(enumerate(products.category.cat.categories))
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
         self.max_length = max_length
@@ -46,7 +52,7 @@ class TextDataset(Dataset):
         category = example['category']
         label_code = self.data.category.cat.categories.get_loc(category)
         label = torch.as_tensor(label_code)
-        sentence = example['product_description']
+        sentence = example['combined']
 
         encoded = self.tokenizer.batch_encode_plus([sentence],
             max_length=self.max_length, padding='max_length', truncation=True)
@@ -64,11 +70,12 @@ class TextDataset(Dataset):
 if __name__ == "__main__":
     dataset = TextDataset()
 
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=12, shuffle=False, num_workers=1)
+    dataloader = DataLoader(dataset, batch_size=12, shuffle=False, num_workers=1)
 
     for i, (data, labels) in enumerate(dataloader):
-        print(data)
-        print(labels)
-        print(data.size())
+        for sentence in data:
+            print(sentence)
         if i == 0:
             break
+
+    # print(dataset.data.info())
