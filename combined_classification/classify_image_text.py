66e0e55c01094c27376f3c91246e6cc79f24f892
ed <edward.webb@outlook.com>
2022-11-29 15:22:16 +0000

    refactoring

--- combined_classification/classify_image_text.py
+++ combined_classification/classify_image_text.py
@@ -1,7 +1,9 @@
 import copy
 import torch
 from torch.utils.data import DataLoader
-from torch.utils.data import random_split 
+from torch.utils.data import random_split
+from torch.utils.tensorboard.writer import SummaryWriter
+import torch.nn as nn
 
 from combined_classification.image_text_dataset import ImageTextDataset
 from combined_classification.image_text_model import ImageTextClassifier
@@ -14,90 +16,103 @@ momentum=0.9
 shuffle = True
 num_workers=1
 pin_memory=True
+max_length = 200
 model = ImageTextClassifier(num_classes = 13)
 model = model.to(device)
 optimiser = torch.optim.Adam([*model.image_classifier.parameters(), *model.text_classifier.main.parameters(), *model.combiner.parameters()], lr=lr)
-# optimiser = torch.optim.Adam((model.text_classifier.main.parameters(), model.combiner.parameters()), lr=lr)
 
 criterion = nn.CrossEntropyLoss()
 criterion = criterion.to(device)
-# exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimiser, step_size=7, gamma=0.9)
 
-dataset = ImageTextDataset(labels_level=0, max_length=100, read_csv=True)
+dataset = ImageTextDataset(labels_level=0, max_length=max_length, read_csv=True)
 num_train_data = len(dataset) - round((len(dataset) * 0.25))
 num_val_data = len(dataset) - num_train_data
 train_set, validation_set = random_split(dataset,[num_train_data, num_val_data])
 train_loader = DataLoader(dataset=train_set, shuffle=shuffle, batch_size=batch_size,num_workers=num_workers,pin_memory=pin_memory)
 val_loader = DataLoader(dataset=validation_set, shuffle=shuffle, batch_size=batch_size,num_workers=num_workers, pin_memory=pin_memory)
 
-def train():
-    best_img_w = copy.deepcopy(model.image_classifier.state_dict())
-    best_text_w = copy.deepcopy(model.text_classifier.main.state_dict())
-    best_comb_w = copy.deepcopy(model.combiner.state_dict())
-    best_acc = 0.0
+title = f"imagetext_lr={lr}, batch_size={batch_size}, num_epochs={num_epochs}, max_length={max_length}"
+layout = {
+    "Loss and Accuracy": {
+        "loss": ["Multiline", ["loss/training", "loss/validation"]],
+        "accuracy": ["Multiline", ["accuracy/training", "accuracy/validation"]],
+    },
+}
+writer = SummaryWriter(f"drive/MyDrive/runs/imagetext_lr={lr}, batch_size={batch_size}, num_epochs={num_epochs}, max_length={max_length}")
+writer.add_custom_scalars(layout)
 
-    for epoch in range(num_epochs):
-        print("-~-" * 30)  
-        print(f"Epoch [{epoch + 1}/{num_epochs}]")
-        print("-~-" * 30)        
 
-        trn_run_loss2 = 0.0
-        trn_run_loss = 0.0
-        trn_run_corr = 0
-        # Training loop.
-        for images, text, labels in train_loader:
-            model.train()
+def train_model(mode:str, dataloader:DataLoader):
+    running_loss = 0.0
+    running_correct = 0
+    if mode == "train":
+        model.train()
+    else:
+        model.eval()
+    with torch.set_grad_enabled(mode == "train"):
+        for images, text, labels in dataloader:
             images = images.to(device)
             text = text.to(device)
             labels = labels.to(device)
 
-            optimiser.zero_grad()
-            torch.set_grad_enabled(True)
+            if mode == "train": 
+                optimiser.zero_grad()
+
             outputs = model(images, text)
             _, preds = torch.max(outputs, 1)
             loss = criterion(outputs, labels)
 
-            loss.backward()
-            optimiser.step()
+            if mode == "train":
+                loss.backward()
+                optimiser.step()
+            
+            running_loss += loss.item() * images.size(0)
+            running_correct += torch.sum(preds == labels.data)
+        
+    return running_loss, running_correct
 
-            trn_run_loss2 += loss.item()
-            trn_run_loss += loss.item() * images.size(0)
-            trn_run_corr += torch.sum(preds == labels.data)
+def calc_epoch_loss(running_loss: float, num_data: int) -> float:
+    epoch_loss = running_loss / num_data
+    return epoch_loss
 
-        # exp_lr_scheduler.step()
-        trn_epoch_loss = trn_run_loss / num_train_data
-        trn_epoch_acc = (trn_run_corr.double() / num_train_data) * 100
+def calc_epoch_acc(running_correct, num_data: int) -> float:
+    epoch_acc = (running_correct.double() / num_data) * 100
+    return epoch_acc
+
+def write_to_summary_writer(mode, loss, accuracy, epoch):
+    writer.add_scalar(f"loss/{mode}", loss, epoch)
+    writer.add_scalar(f"accuracy/{mode}", accuracy, epoch)
+
+def save_best_weights():
+    best_img_w = copy.deepcopy(model.image_classifier.state_dict())
+    best_text_w = copy.deepcopy(model.text_classifier.main.state_dict())
+    best_comb_w = copy.deepcopy(model.combiner.state_dict())
+    return best_img_w, best_text_w, best_comb_w
+
+def main_loop():
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print("-~-" * 30, f"\nEpoch [{epoch + 1}/{num_epochs}]\n", "-~-" * 30)      
+
+        # Training loop.
+        trn_run_loss, trn_run_corr = train_model("train", train_loader)
+        trn_epoch_loss = calc_epoch_loss(trn_run_loss, num_train_data)
+        trn_epoch_acc = calc_epoch_acc(trn_run_corr, num_train_data)
+        write_to_summary_writer("training", trn_epoch_loss, trn_epoch_acc, epoch)
         print(f' Epoch [{epoch + 1}/{num_epochs}] TRAINING Loss: {trn_epoch_loss:.4f}, Acc: {trn_epoch_acc:.4f}%')
 
-        val_run_loss = 0.0
-        val_run_corr = 0
-        correct = 0.0
-        # Eval loop
-        with torch.no_grad():
-            for features, labels in val_loader:
-                model.eval()
-                features = features.to(device)
-                labels = labels.to(device)
-                
-                outputs = model(features)
-                _, preds = torch.max(outputs, 1)
-                loss = criterion(outputs, labels)
-
-                val_run_loss += loss.item() * features.size(0)
-                val_run_corr += torch.sum(preds == labels.data)
-                correct += (preds == labels).sum().item()
-        
-        val_epoch_loss = val_run_loss / num_val_data
-        val_epoch_acc = (val_run_corr.double() / num_val_data) * 100
+        # Eval loop.
+        val_loss, val_correct = train_model("eval", val_loader)
+        val_epoch_loss = calc_epoch_loss(val_loss, num_val_data)
+        val_epoch_acc = calc_epoch_acc(val_correct, num_val_data)
+        write_to_summary_writer("validation", val_epoch_loss, val_epoch_acc)
         print(f' Epoch [{epoch + 1}/{num_epochs}] VALIDATION Loss: {val_epoch_loss:.4f}, Acc: {val_epoch_acc:.4f}%')
 
         if val_epoch_acc > best_acc:
             best_acc = val_epoch_acc
-            best_img_w = copy.deepcopy(model.image_classifier.state_dict())
-            best_text_w = copy.deepcopy(model.text_classifier.main.state_dict())
-            best_comb_w = copy.deepcopy(model.combiner.state_dict())
-            print(f"New best accuracy! {best_acc:.4f}")
-        print("\n")
+            best_img_w, best_text_w, best_comb_w = save_best_weights()
+            print(f"New best accuracy! {best_acc:.4f}\n")
 
     print(f"Best val accuracy: {best_acc:4f}")
     model.image_classifier.load_state_dict(best_img_w)
@@ -107,12 +122,11 @@ def train():
             'img_state_dict': model.image_classifier.state_dict(),
             'text_state_dict': model.text_classifier.main.state_dict(),
             'combiner_state_dict': model.combiner.state_dict(),
-            }, 'models/combined_model.pt')
+            }, 'combined_model.pt')
 
 
 if __name__ == '__main__':
     print(device)
-    train()
-
+    main_loop()
 
         
